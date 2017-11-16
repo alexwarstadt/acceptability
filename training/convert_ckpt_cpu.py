094bd00d249fc75082fb167180f52b937a3ad840
Alex Warstadt <alexwarstadt@gmail.com>
2017-11-15 23:01:19 -0500

    convert all models to cpu

--- training/convert_ckpt_cpu.py
+++ training/convert_ckpt_cpu.py
@@ -1,14 +1,18 @@
 import models.rnn_classifier
 import torch
+import os
 
-encoder = models.rnn_classifier.ClassifierPooling(
-    hidden_size=748,
-    embedding_size=300,
-    num_layers=3)
+for model in os.listdir("/scratch/asw462/models/"):
+    h_size = int(filter(lambda s: s.startswith("h_size"), model.split("-"))[0].replace("h_size", ""))
+    num_layers = int(filter(lambda s: s.startswith("num_layers"), model.split("-"))[0].replace("num_layers", ""))
 
-encoder.load_state_dict(torch.load(
-    '/scratch/asw462/models/sweep_1106235815_rnn_classifier_pooling_14-lr0.00088-h_size748-datadiscriminator-num_layers3'))
-encoder.cpu()
-torch.save(encoder.state_dict(),
-           '/scratch/asw462/models/CPU_sweep_1106235815_rnn_classifier_pooling_14-lr0.00088-h_size748-datadiscriminator-num_layers3')
+    encoder = models.rnn_classifier.ClassifierPooling(
+        hidden_size=h_size,
+        embedding_size=300,
+        num_layers=num_layers)
+
+    encoder.load_state_dict(torch.load(
+        '/scratch/asw462/models/' + model))
+    encoder.cpu()
+    torch.save(encoder.state_dict(), '/scratch/asw462/models/CPU_' + model)
 
