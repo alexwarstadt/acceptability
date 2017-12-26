ea1f723b8f7bce52fbfed30e4f39e3659846d536
Alex Warstadt <alexwarstadt@gmail.com>
2017-12-25 21:47:41 -0500

    cpu-script

--- training/convert_ckpt_cpu.py
+++ training/convert_ckpt_cpu.py
@@ -2,17 +2,29 @@ import models.rnn_classifier
 import torch
 import os
 
-for model in os.listdir("/scratch/asw462/models/"):
-    h_size = int(filter(lambda s: s.startswith("h_size"), model.split("-"))[0].replace("h_size", ""))
-    num_layers = int(filter(lambda s: s.startswith("num_layers"), model.split("-"))[0].replace("num_layers", ""))
 
-    encoder = models.rnn_classifier.ClassifierPooling(
-        hidden_size=h_size,
-        embedding_size=300,
-        num_layers=num_layers)
+dirs = ["sweep_1124013946",
+        "sweep_1124_early_stop_day4",
+        "sweep_121804",
+        "sweep_1106002002",
+        "sweep_1115230657",
+        "sweep_1116160716",
+        "sweep_1124_early_stop_day2.5",
+        "sweep_121800"]
 
-    encoder.load_state_dict(torch.load(
-        '/scratch/asw462/models/' + model))
-    encoder.cpu()
-    torch.save(encoder.state_dict(), '/scratch/asw462/models/CPU_' + model)
+for dir in dirs:
+    for model in os.listdir("/scratch/asw462/models/" + dir):
+
+        h_size = int(filter(lambda s: s.startswith("h_size"), model.split("-"))[0].replace("h_size", ""))
+        num_layers = int(filter(lambda s: s.startswith("num_layers"), model.split("-"))[0].replace("num_layers", ""))
+
+        encoder = models.rnn_classifier.ClassifierPooling(
+            hidden_size=h_size,
+            embedding_size=300,
+            num_layers=num_layers)
+
+        encoder.load_state_dict(torch.load(
+            '/scratch/asw462/models/' + dir + model))
+        encoder.cpu()
+        torch.save(encoder.state_dict(), '/scratch/asw462/models/' + dir + '/CPU_' + model)
 
