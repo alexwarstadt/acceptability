084783bc1d393243b259263e1fabafe541076c9c
Alex Warstadt <alexwarstadt@gmail.com>
2018-01-03 13:32:54 -0500

    debug

--- training/convert_ckpt_cpu_aj.py
+++ training/convert_ckpt_cpu_aj.py
@@ -8,25 +8,25 @@ dirs = ["sweep_1229145144",
         "sweep_1227010500",
         "sweep_1229132558"]
 
-for dir in dirs:
-    for model in os.listdir("/scratch/asw462/models/" + dir + "/"):
-
-        h_size = int(filter(lambda s: s.startswith("h_size"), model.split("-"))[0].replace("h_size", ""))
-        enc_size = int(filter(lambda s: s.startswith("h_size"), model.split("-"))[1].replace("h_size", ""))
-
-        encoder = models.acceptability_cl.Classifier(
-            hidden_size=h_size,
-            encoding_size=enc_size)
-
-        print '/scratch/asw462/models/' + dir + "/" + model
-
-        try:
-            encoder.load_state_dict(torch.load(
-                '/scratch/asw462/models/' + dir + "/" + model))
-            encoder.cpu()
-            torch.save(encoder.state_dict(), '/scratch/asw462/models/' + dir + '/CPU_' + model)
-        except KeyError:
-            pass
+# for dir in dirs:
+#     for model in os.listdir("/scratch/asw462/models/" + dir + "/"):
+#
+#         h_size = int(filter(lambda s: s.startswith("h_size"), model.split("-"))[0].replace("h_size", ""))
+#         enc_size = int(filter(lambda s: s.startswith("h_size"), model.split("-"))[1].replace("h_size", ""))
+#
+#         encoder = models.acceptability_cl.Classifier(
+#             hidden_size=h_size,
+#             encoding_size=enc_size)
+#
+#         print '/scratch/asw462/models/' + dir + "/" + model
+#
+#         try:
+#             encoder.load_state_dict(torch.load(
+#                 '/scratch/asw462/models/' + dir + "/" + model))
+#             encoder.cpu()
+#             torch.save(encoder.state_dict(), '/scratch/asw462/models/' + dir + '/CPU_' + model)
+#         except KeyError:
+#             pass
 
 
 
@@ -36,7 +36,7 @@ for dir in dirs:
     for model in os.listdir("/scratch/asw462/models/" + dir + "/"):
 
         h_size = int(filter(lambda s: s.startswith("h_size"), model.split("-"))[0].replace("h_size", ""))
-        max_pool = filter(lambda s: s.startswith("max_pool"), model.split("-"))[1].replace("max_pool", "") == "1"
+        max_pool = filter(lambda s: s.startswith("max_pool"), model.split("-"))[0].replace("max_pool", "") == "1"
 
         encoder = models.cbow.Classifier(
             hidden_size=h_size,
